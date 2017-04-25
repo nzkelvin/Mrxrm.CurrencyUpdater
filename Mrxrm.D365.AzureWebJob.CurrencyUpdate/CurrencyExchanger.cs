7ea31464173f4a359bcb5763c55fe946695b803c
Kelvin Shen <kelvin.shen@fusion5.co.nz>
2017-04-26 10:13:46 +1200

    initialize project to github

---
+++ Mrxrm.D365.AzureWebJob.CurrencyUpdate/CurrencyExchanger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Mrxrm.D365.AzureWebJob.CurrencyUpdate
+{
+    public interface ICurrencyExchanger
+    {
+        Dictionary<string, decimal> GetExchangeRates();
+    }
+
+    public class CurrencyExchanger : ICurrencyExchanger
+    {
+        private readonly string _exchangeApiUrl;
+
+        public CurrencyExchanger()
+        {
+            _exchangeApiUrl = System.Configuration.ConfigurationManager.AppSettings["ExchangeApiUrl"];
+        }
+
+        public Dictionary<string, decimal> GetExchangeRates()
+        {
+            Dictionary<string, decimal> exchangeRates = Task.Run(new Func<Task<Dictionary<string, decimal>>>(async () =>
+                {
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetAsync(_exchangeApiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        JObject jResult = JObject.Parse(result);
+                        var rates = JsonConvert.DeserializeObject<Dictionary<string, decimal>>(jResult["rates"].ToString());
+
+                        return rates;
+                    }
+
+                    return null;
+                }))
+                .GetAwaiter()
+                .GetResult();
+
+            return exchangeRates;
+        }
+    }
+}
---
+++ Mrxrm.D365.AzureWebJob.CurrencyUpdate/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Mrxrm.D365.AzureWebJob.CurrencyUpdate.Models.D365;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Ninject;
+
+namespace Mrxrm.D365.AzureWebJob.CurrencyUpdate
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        // todo: !important! Those two connection strings should be manually added to Azure portal.
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            Console.WriteLine("Start Currency Update");
+
+            //IoC
+            var kernel = new Ninject.StandardKernel();
+            kernel.Bind<ICurrencyExchanger>().To<CurrencyExchanger>();
+            kernel.Bind<IOrganizationService>().ToConstructor(x => new OrganizationService(x.Inject<string>())).WithConstructorArgument("connectionStringName", "Xrm");
+            kernel.Bind<ICrmServiceContext>().To<CrmServiceContext>().WithConstructorArgument("service", kernel.Get<IOrganizationService>());
+
+            
+            Execute(kernel.Get<ICurrencyExchanger>(), kernel.Get<ICrmServiceContext>());
+        }
+
+        public static void Execute(ICurrencyExchanger exchanger, ICrmServiceContext ctx)
+        {
+            // Get Open exchange rates
+            var exchangeRates = exchanger.GetExchangeRates();
+
+            // Nice to have logging code. Indicates if OpenExchange.org is working or not.
+            foreach (var rate in exchangeRates)
+            {
+                Console.WriteLine($"{rate.Key}: {rate.Value}");
+            }
+
+            // Get base currency
+            var baseIso = ctx.OrganizationSet
+                .Join(ctx.TransactionCurrencySet, o => o.BaseCurrencyId.Id, c => c.TransactionCurrencyId,
+                    (o, c) => new {o, c})
+                .Select(j => j.c.ISOCurrencyCode)
+                .First();
+
+            Console.WriteLine($"Base ISO: {baseIso}");
+
+            decimal baseCurrencyRate = exchangeRates[baseIso];
+
+            var currencies = ctx.TransactionCurrencySet
+                .Select(c => new TransactionCurrency()
+                {
+                    TransactionCurrencyId = c.TransactionCurrencyId,
+                    ISOCurrencyCode = c.ISOCurrencyCode
+                }).ToArray();
+
+            foreach (var c in currencies)
+            {
+                if (c.ISOCurrencyCode == baseIso) continue;
+
+                var rate = exchangeRates[c.ISOCurrencyCode] / baseCurrencyRate;
+
+                ctx.Execute(new UpdateRequest()
+                {
+                    Target = new TransactionCurrency()
+                    {
+                        TransactionCurrencyId = c.TransactionCurrencyId,
+                        ExchangeRate = rate
+                    }
+                });
+
+                Console.WriteLine($"Update D365 currency {c.ISOCurrencyCode} exchange rate to {rate}");
+            }
+        }
+    }
+}
